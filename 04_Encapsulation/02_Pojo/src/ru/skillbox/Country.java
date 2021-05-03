9e965e7b7bc88ffe8174c5836858798a71a6380f
Roman Vladimirov <mxhx23@gmail.com>
2021-05-03 23:39:17 +0300

    45 - 1 задание

---
+++ 04_Encapsulation/02_Pojo/src/ru/skillbox/Country.java
@@ -0,0 +1,54 @@
+package ru.skillbox;
+
+public class Country {
+
+    private String nameCountry;
+    private String capitalName;
+    private double area;
+    private int population;
+    private boolean landlocked;
+
+    public Country(String nameCountry) {
+        this.nameCountry = nameCountry;
+    }
+
+    public String getNameCountry() {
+        return nameCountry;
+    }
+
+    public void setNameCountry(String nameCountry) {
+        this.nameCountry = nameCountry;
+    }
+
+    public String getCapitalName() {
+        return capitalName;
+    }
+
+    public void setCapitalName(String capitalName) {
+        this.capitalName = capitalName;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public boolean isLandlocked() {
+        return landlocked;
+    }
+
+    public void setLandlocked(boolean landlocked) {
+        this.landlocked = landlocked;
+    }
+}
