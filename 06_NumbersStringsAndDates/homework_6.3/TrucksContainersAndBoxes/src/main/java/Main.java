9e0a33c5f099663614b0f4ad6cac63c40ed2c9c9
Roman <mxhx23@gmail.com>
2021-05-16 22:10:39 +0300

    6.3 - задание 2

--- 06_NumbersStringsAndDates/homework_6.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -5,20 +5,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String boxes = scanner.nextLine();
-
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
-        // пример вывода при вводе 2
-        // для отступа используйте табуляцию - \t
-
-        /*
-        Грузовик: 1
-            Контейнер: 1
-                Ящик: 1
-                Ящик: 2
-        Необходимо:
-        грузовиков - 1 шт.
-        контейнеров - 1 шт.
-        */
+        CountingBoxes(boxes);
     }
-
+    public static void CountingBoxes(String boxes){
+        int boxCount = Integer.parseInt(boxes);
+        int containerCount = 0;
+        int trackCount = 0;
+        int countForBox = 1;
+        int countForContainer = 1;
+        if(boxCount > 0) {
+            if(boxCount == 27) {
+                containerCount = 1;
+            }else {
+                containerCount = boxCount % 27 == 0 ? boxCount % 27 : boxCount / 27 + 1;
+            }
+        }
+        if (containerCount > 0){
+            trackCount = containerCount % 12 == 0 ? containerCount % 12 : containerCount / 12 + 1;
+        }
+        for (int a = 1; a <= trackCount; a++){
+            System.out.println("Грузовик: " + a);
+            for (int b = 1; b <= 12 && countForContainer <= containerCount; b++){
+                System.out.println("\tКонтейнер: " + countForContainer);
+                countForContainer++;
+                for (int c = 1; c <= 27 && countForBox <= boxCount; c++){
+                    System.out.println("\t\tЯщик: " + countForBox);
+                    countForBox++;
+                }
+            }
+        }
+        System.out.println("Необходимо:");
+        System.out.println("грузовиков - " + trackCount + " шт.");
+        System.out.println("контейнеров - " + containerCount + " шт.");
+    }
 }
