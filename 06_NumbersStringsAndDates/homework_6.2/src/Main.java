fd130cbea23fd4ff29d3dd918f920fb0fb401c14
Roman <mxhx23@gmail.com>
2021-05-16 14:17:15 +0300

    6.2 исправил

--- 06_NumbersStringsAndDates/homework_6.2/src/Main.java
+++ 06_NumbersStringsAndDates/homework_6.2/src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+
         System.out.println("byte(max, min): " + Byte.MAX_VALUE + ", " + Byte.MIN_VALUE);
         System.out.println("short(max, min): " + Short.MAX_VALUE + ", " + Short.MIN_VALUE);
         System.out.println("int(max, min): " + Integer.MAX_VALUE + ", " + Integer.MIN_VALUE);
         System.out.println("long(max, min): " + Long.MAX_VALUE + ", " + Long.MIN_VALUE);
-        System.out.println("float(max, min): " + Float.MAX_VALUE + ", " + Float.MIN_VALUE);
-        System.out.println("double(max, min): " + Double.MAX_VALUE + ", " + Double.MIN_VALUE);
+        System.out.println("float(max, min): " + Float.MAX_VALUE + ", " + -Float.MIN_VALUE);
+        System.out.println("double(max, min): " + Double.MAX_VALUE + ", " + -Double.MIN_VALUE);
 
     }
 }
