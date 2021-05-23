eb8098d505d29ecbcc6c63d6149720d54259d00b
Roman <mxhx23@gmail.com>
2021-05-23 16:07:39 +0300

    6.4 - 1 задание

--- 06_NumbersStringsAndDates/homework_6.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -2,8 +2,42 @@ public class Main {
 
   public static void main(String[] args) {
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
-  }
+    int firstNumber = 0;
+    int zeroCount = 0;
+    int ifCount = 0;
+    int textSum = 0;
 
+    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+
+    for (int i = 0; i < text.length(); i++){
+      if (Character.toString(text.charAt(i)).equals(" ")){
+        zeroCount++;
+      }
+      if (zeroCount == 2 && ifCount == 0){
+          firstNumber = ++i;
+          ifCount++;
+      }
+      if (zeroCount == 3 && ifCount == 1 ){
+        textSum += Integer.parseInt(text.substring(firstNumber, i));
+        ifCount++;
+      }
+      if (zeroCount == 6 && ifCount == 2){
+        firstNumber = ++i;
+        ifCount++;
+      }
+      if (zeroCount == 7 && ifCount == 3 ){
+        textSum += Integer.parseInt(text.substring(firstNumber, i));
+        ifCount++;
+      }
+      if (zeroCount == 11 && ifCount == 4){
+        firstNumber = ++i;
+        ifCount++;
+      }
+      if (zeroCount == 12 && ifCount == 5 ){
+        textSum += Integer.parseInt(text.substring(firstNumber, i));
+        System.out.println(textSum);
+        ifCount++;
+      }
+    }
+  }
 }
