2cdec668c4947829b63225a23f8d949bd263ff29
Roman <mxhx23@gmail.com>
2021-05-24 19:54:40 +0300

    6.4 - 3 задание

--- 06_NumbersStringsAndDates/homework_6.4/FullNameFormatter/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.4/FullNameFormatter/src/main/java/Main.java
@@ -5,14 +5,62 @@ public class Main {
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
+
     while (true) {
+
+      int firstSpaceCheck = 0;
+      int firstCount = 0;
+      int secondCount = 0;
+      int stringCount = 0;
+
+      boolean isNumeric = false;
+
+      String lastName;
+      String firstName;
+      String patronymic;
       String input = scanner.nextLine();
       if (input.equals("0")) {
         break;
+      }else {
+        for (int i = 0; i < input.length(); i++) {
+
+          if (Character.isDigit(input.charAt(i))){
+            isNumeric = true;
+          }
+          if (Character.toString(input.charAt(i)).equals(" ")) {
+
+            stringCount += 1;
+
+            if (stringCount == 1 && firstSpaceCheck == 0){
+              firstCount = i;
+              firstSpaceCheck++;
+            }
+            if (stringCount == 2 && firstSpaceCheck == 1){
+              secondCount = i;
+              firstSpaceCheck++;
+            }
+          }
+        }
+
+        if (stringCount == 0 || stringCount == 3 || isNumeric){
+          System.out.println("Введенная строка не является ФИО");
+          continue;
+        }
+
+        if (stringCount == 1 && !isNumeric) {
+          lastName = input.substring(0, firstCount);
+          firstName = input.substring(++firstCount);
+          System.out.println("Фамилия: " + lastName + "\n" + "Имя: " + firstName);
+          continue;
+        }
+        if (stringCount == 2 && !isNumeric ) {
+          lastName = input.substring(0, firstCount);
+          firstName = input.substring(++firstCount, secondCount);
+          patronymic = input.substring(++secondCount);
+          System.out.println("Фамилия: " + lastName + "\n" + "Имя: " + firstName + "\n" + "Отчество: " + patronymic);
+          continue;
+        }
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
     }
   }
-
 }
