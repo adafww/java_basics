609215c4bf55e8af3dbb78cebc5ac9a73a62d90a
Roman <mxhx23@gmail.com>
2021-05-24 22:40:23 +0300

    6.4 - 4 задание

--- 06_NumbersStringsAndDates/homework_6.4/MaskingInformation/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.4/MaskingInformation/src/main/java/Main.java
@@ -5,8 +5,55 @@ public class Main {
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+
+        int firstBracketLeft = 0;
+        int secondBracketLeft = 0;
+        int firstBracketRight = 0;
+        int secondBracketRight = 0;
+        int firstCount = 0;
+        int secondCount = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+
+            if (Character.toString(text.charAt(i)).equals("<")) {
+
+                firstCount++;
+                if (firstCount == 2){
+
+                    secondBracketLeft = i;
+                }
+                if (firstCount == 1){
+
+                    firstBracketLeft = i;
+                }
+            }
+
+            if (Character.toString(text.charAt(i)).equals(">")) {
+
+                secondCount++;
+                if (secondCount == 2){
+
+                    secondBracketRight = i;
+                }
+                if (secondCount == 1){
+
+                    firstBracketRight = i;
+                }
+            }
+        }
+
+        if (firstCount == 1 && secondCount == 1){
+
+            text = text.substring(0, firstBracketLeft) + placeholder +
+                    text.substring(++firstBracketRight);
+        }
+
+        if (firstCount == 2 && secondCount == 2){
+
+            text = text.substring(0, firstBracketLeft) + placeholder +
+                    text.substring(++firstBracketRight, secondBracketLeft) + placeholder +
+                    text.substring(++secondBracketRight);
+        }
         return text;
     }
-
 }
