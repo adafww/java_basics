5fab9e84202e13acfa1ce7d722e22f378cc953c2
Roman <mxhx23@gmail.com>
2021-05-27 00:39:23 +0300

    6.5 - 1 задание

--- 06_NumbersStringsAndDates/homework_6.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -2,11 +2,42 @@ public class Main {
 
   public static void main(String[] args) {
 
+    System.out.println(calculateSalarySum("Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей"));
   }
 
   public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
+
+    int[] digitCount = new int[text.length()];
+    boolean countSwitch = false;
+    int count = 0;
+    int result = 0;
+
+    for (int i = 0; i < text.length(); i++){
+
+      if(!countSwitch && Character.toString(text.charAt(i)).matches("[0-9]")){
+
+        digitCount[count] = i;
+        count++;
+        countSwitch = true;
+      }
+
+      if(countSwitch && !Character.toString(text.charAt(i)).matches("[0-9]")){
+
+        digitCount[count] = i;
+        count++;
+        countSwitch = false;
+      }
+    }
+
+    for (int i = 0; i < digitCount.length; i++ ){
+
+      if (digitCount[i] != 0) {
+
+        result += Integer.parseInt(text.substring(digitCount[i], digitCount[++i]));
+      }
+    }
+
+    return result;
   }
 
 }
