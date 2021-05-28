6071c236e577baed282445d62c46b3a4f26b5986
Roman <mxhx23@gmail.com>
2021-05-28 17:05:16 +0300

    6.5 - 5 задание

--- 06_NumbersStringsAndDates/homework_6.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -5,8 +5,34 @@ public class Main {
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
-        return text;
-    }
 
+        int[] brackets = new int[text.length()];
+        int count = 0;
+        boolean countSwitch = false;
+        String result = "";
+
+        for (int i = 0; i < text.length(); i++) {
+
+            if (!countSwitch && Character.toString(text.charAt(i)).matches("<")) {
+
+                brackets[count] = i;
+                count++;
+                countSwitch = true;
+            }
+
+            if (countSwitch && Character.toString(text.charAt(i)).matches(">")) {
+
+                brackets[count] = i;
+                count++;
+                countSwitch = false;
+            }
+        }
+
+        for (int i = 0; i < text.length(); i++){
+
+            result += text.substring(brackets[i]) + placeholder + text.substring(brackets[++i]);
+        }
+
+        return result;
+    }
 }
