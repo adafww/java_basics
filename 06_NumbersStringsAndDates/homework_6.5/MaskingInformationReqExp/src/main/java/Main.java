1c41def78b22d0f83c519fce978988275df0d3d0
Roman <mxhx23@gmail.com>
2021-06-08 10:12:21 +0300

    6.5 - 5 задание исправил

--- 06_NumbersStringsAndDates/homework_6.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -6,32 +6,7 @@ public class Main {
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
 
-        int[] brackets = new int[text.length()];
-        int count = 0;
-        boolean countSwitch = false;
-        String result = "";
-
-        for (int i = 0; i < text.length(); i++) {
-
-            if (!countSwitch && Character.toString(text.charAt(i)).matches("<")) {
-
-                brackets[count] = i;
-                count++;
-                countSwitch = true;
-            }
-
-            if (countSwitch && Character.toString(text.charAt(i)).matches(">")) {
-
-                brackets[count] = i;
-                count++;
-                countSwitch = false;
-            }
-        }
-
-        for (int i = 0; i < text.length(); i++){
-
-            result += text.substring(brackets[i]) + placeholder + text.substring(brackets[++i]);
-        }
+        String result = text.replaceAll("<[0-9]+>|<[\\s0-9]+>", placeholder);
 
         return result;
     }
