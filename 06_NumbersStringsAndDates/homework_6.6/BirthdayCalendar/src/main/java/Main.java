50975555039be23d879deb522b52f5909dc388b4
Roman <mxhx23@gmail.com>
2021-06-12 19:27:43 +0300

    6.6 - дз

--- 06_NumbersStringsAndDates/homework_6.6/BirthdayCalendar/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.6/BirthdayCalendar/src/main/java/Main.java
@@ -1,22 +1,41 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int day = 31;
-        int month = 12;
-        int year = 1990;
+        int day = 15;
+        int month = 9;
+        int year = 1995;
 
         System.out.println(collectBirthdays(year, month, day));
-
     }
 
     public static String collectBirthdays(int year, int month, int day) {
 
-        //TODO реализуйте метод для построения строки в следующем виде
-        //0 - 31.12.1990 - Mon
-        //1 - 31.12.1991 - Tue
-        
-        return "";
+        LocalDate birthDay = LocalDate.of(year, month, day);
+
+        LocalDate currentTime = LocalDate.now();
+
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy - E", Locale.US);
+
+        String result = "";
+
+        for (int i = 0; i <= Period.between(birthDay, currentTime).getYears(); i++){
+
+            if(Period.between(birthDay, currentTime).getMonths() >= 0 && Period.between(birthDay, currentTime).getDays() >= 0){
+
+                result += i + " - " + format.format(birthDay.plusYears(i)) + System.lineSeparator();
+            }
+        }
+
+        return result;
     }
 }
+
+//TODO реализуйте метод для построения строки в следующем виде
+//0 - 31.12.1990 - Mon
+//1 - 31.12.1991 - Tue
