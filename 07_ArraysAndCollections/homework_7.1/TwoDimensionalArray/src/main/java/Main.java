7f8ba32fdc67db41caec8558f2fa448b4b7449ac
Roman <mxhx23@gmail.com>
2021-06-13 17:01:49 +0300

    7.1 - 3 задание

--- 07_ArraysAndCollections/homework_7.1/TwoDimensionalArray/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.1/TwoDimensionalArray/src/main/java/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+
+        char[][] result = TwoDimensionalArray.getTwoDimensionalArray(3);
+
+        for (int i = 0; i < result.length; i++) {
+
+            for (int j = 0; j < result[i].length; j++) {
+
+                System.out.print(result[i][j] + "\t");
+            }
+
+            System.out.println();
+        }
         //Распечатайте сгенерированный в классе TwoDimensionalArray.java двумерный массив
     }
 }
--- 07_ArraysAndCollections/homework_7.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
+++ 07_ArraysAndCollections/homework_7.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
@@ -9,6 +9,34 @@ public class TwoDimensionalArray {
         // [ , X,  ]
         // [X,  , X]
 
-        return new char[0][0];
+        char[][] result = new char[size][size];
+        int a = 0;
+        int b = 0;
+        int x = 0;
+        int y = size - 1;
+
+        for (int i = 0; i < size; i++){
+
+            for (int j = 0; j < size; j++){
+
+                result[i][j] = ' ';
+
+                if(i == a && j == b){
+
+                    result[i][j] = symbol;
+                    b++;
+                    a++;
+                }
+
+                if(i == x && j == y){
+
+                    result[i][j] = symbol;
+                    x++;
+                    y--;
+                }
+            }
+        }
+
+        return result;
     }
 }
