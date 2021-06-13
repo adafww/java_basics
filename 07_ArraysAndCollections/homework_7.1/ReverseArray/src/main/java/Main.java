fd5228855110d8db82a2dae429f57f250929fade
Roman <mxhx23@gmail.com>
2021-06-13 12:27:14 +0300

    7.1 - 1 задание

--- 07_ArraysAndCollections/homework_7.1/ReverseArray/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.1/ReverseArray/src/main/java/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
+
         String line = "Каждый охотник желает знать, где сидит фазан";
+        String[] text = line.split(",?\\s");
+        String[] reverseArray = ReverseArray.reverse(text);
+
+        for (int i = 0; i < reverseArray.length; i++){
+
+            System.out.println(reverseArray[i]);
+        }
     }
 }
