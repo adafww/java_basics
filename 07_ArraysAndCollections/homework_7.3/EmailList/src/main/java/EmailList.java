901d5633e93698d3c970f96ef7f244ebae1b40d2
Roman <mxhx23@gmail.com>
2021-07-15 22:16:35 +0300

    7.3 - задание

--- 07_ArraysAndCollections/homework_7.3/EmailList/src/main/java/EmailList.java
+++ 07_ArraysAndCollections/homework_7.3/EmailList/src/main/java/EmailList.java
@@ -3,15 +3,23 @@ import java.util.*;
 public class EmailList {
 
     TreeSet<String> emailList = new TreeSet<>();
+    String regex = "([-._A-Za-z0-9]+[@][A-Za-z]+[.][A-Za-z]+)";
 
     public void add(String email) {
 
-        emailList.add(email.toLowerCase(Locale.ROOT));
+        if(email.matches(regex)){
+
+            emailList.add(email.toLowerCase(Locale.ROOT));
+        }else {
+
+            System.out.println(Main.WRONG_EMAIL_ANSWER);
+        }
+
     }
 
-    public TreeSet<String> getSortedEmails() {
+    public List<String> getSortedEmails() {
 
-        return emailList;
+        return new ArrayList<>(emailList);
     }
 
 }
--- 07_ArraysAndCollections/homework_7.3/EmailList/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.3/EmailList/src/main/java/Main.java
@@ -5,42 +5,21 @@ public class Main {
 
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
     private static EmailList emailList = new EmailList();
-    
-    /* TODO:
-        Пример вывода списка Email, после ввода команды LIST в консоль:
-        test@test.com
-        hello@mail.ru
-        - каждый адрес с новой строки
-        - список должен быть отсортирован по алфавиту
-        - email в разных регистрах считается одинаковыми
-           hello@skillbox.ru == HeLLO@SKILLbox.RU
-        - вывод на печать должен быть в нижнем регистре
-           hello@skillbox.ru
-        Пример вывода сообщения об ошибке при неверном формате Email:
-        "Неверный формат email"
-    */
 
     public static void main(String[] args) {
 
-        String regex = "^(ADD|LIST|)(\\s|)([-._A-Za-z0-9]+|)([@]|)([A-Za-z]+|)(.|)([A-Za-z]+|)";
+        String regex = "^(ADD|LIST)(\\s)([-._A-Za-z0-9]+[@][A-Za-z]+[.][A-Za-z]+)";
         Scanner scanner = new Scanner(System.in);
         
         while (true) {
             String input = scanner.nextLine();
-            String pattern = Pattern.compile(regex).matcher(input).replaceAll("$1");
+            String patternAddList = Pattern.compile(regex).matcher(input).replaceAll("$1");
             if (input.equals("0")) {
                 break;
-            }else if(pattern.equals("ADD") &&
-                    !Pattern.compile(regex).matcher(input).replaceAll("$2").equals("") &&
-                    !Pattern.compile(regex).matcher(input).replaceAll("$3").equals("") &&
-                    Pattern.compile(regex).matcher(input).replaceAll("$4").equals("@") &&
-                    !Pattern.compile(regex).matcher(input).replaceAll("$5").equals("") &&
-                    Pattern.compile(regex).matcher(input).replaceAll("$6").equals(".") &&
-                    !Pattern.compile(regex).matcher(input).replaceAll("$7").equals("")){
+            }else if(patternAddList.equals("ADD")){
 
-                emailList.add(
-                        Pattern.compile(regex).matcher(input).replaceAll("$3$4$5$6$7"));
-            }else if(pattern.equals("LIST")){
+                emailList.add(Pattern.compile(regex).matcher(input).replaceAll("$3"));
+            }else if(patternAddList.equals("LIST")){
 
 
                 for(String list : emailList.emailList){
