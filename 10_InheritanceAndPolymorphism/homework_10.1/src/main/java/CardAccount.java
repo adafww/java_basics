a50157973a83fbaae05f2b588e0784c2de5770ac
Roman <mxhx23@gmail.com>
2021-07-28 08:18:23 +0300

    10.1 - задание

--- 10_InheritanceAndPolymorphism/homework_10.1/src/main/java/CardAccount.java
+++ 10_InheritanceAndPolymorphism/homework_10.1/src/main/java/CardAccount.java
@@ -3,7 +3,7 @@ public class CardAccount extends BankAccount {
     @Override
     public boolean take(double amountToTake) {
 
-        if (amountToTake <= bill){
+        if (amountToTake * 1.01 <= bill){
 
             bill -= amountToTake * 1.01;
             return true;
