268a472a248e500e7cc1c9dfa1b41e1fb4fe1894
Roman <mxhx23@gmail.com>
2021-09-02 17:40:29 +0300

    11.2 задание

--- 11_AdvancedOOPFeatures/homework_11.2/Airport/src/main/java/Main.java
+++ 11_AdvancedOOPFeatures/homework_11.2/Airport/src/main/java/Main.java
@@ -1,57 +1,26 @@
-import com.skillbox.airport.Aircraft;
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import net.sf.saxon.pattern.LocalNameTest;
-
-import javax.swing.plaf.basic.BasicDesktopIconUI;
-import java.sql.Time;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        Airport airport = Airport.getInstance();
-        LocalDate localDate = LocalDate.now();
-        LocalTime localTime = LocalTime.now().minusHours(2);
-        Date date = new Date(localDate.getYear()
-                ,localDate.getMonthValue()
-                ,localDate.getDayOfMonth()
-                ,localTime.getHour()
-                ,localTime.getMinute()
-                ,localTime.getSecond()
-        );
-        airport.getTerminals().stream()
-                .map(a -> a.getFlights().stream().map(b -> b.getDate().getTime()).filter(c -> c >= date.getTime()))
-                .forEach(System.out::println);
+        findPlanesLeavingInTheNextTwoHours(Airport.getInstance()).stream().forEach(System.out::println);
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
 
-        /*
-        LocalDate localDate = LocalDate.now();
-        LocalTime localTime = LocalTime.now().minusHours(2);
-        Date date = new Date(localDate.getYear()
-                ,localDate.getMonthValue()
-                ,localDate.getDayOfMonth()
-                ,localTime.getHour()
-                ,localTime.getMinute()
-                ,localTime.getSecond()
-        );
-        airport.getTerminals().stream()
-                .map(a -> a.getFlights().stream()
-                .filter(g -> g.getDate().getTime() >= date.getTime()))
+        final int TWOHOURS = 7_200_000;
+        Date date = new Date();
+
+        return airport.getTerminals().stream()
+                .flatMap(a -> a.getFlights().stream())
+                .filter(g -> g.getDate().getTime() < (date.getTime() + TWOHOURS)
+                        && g.getDate().getTime() >= date.getTime()
+                        && g.getType().equals(Flight.Type.DEPARTURE))
                 .collect(Collectors.toList());
-
-         */
-        return new ArrayList<>();
     }
-
 }
