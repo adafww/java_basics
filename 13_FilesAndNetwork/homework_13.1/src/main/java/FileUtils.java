e68c8299b0766d0e89bed94e2eb1a91356ca6a76
Roman <mxhx23@gmail.com>
2021-09-11 21:07:18 +0300

    13.1 задание

--- 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
@@ -1,6 +1,29 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+
+        Path paths = Paths.get(path);
+        long size = 0;
+
+        try (Stream<Path> init = Files.walk(paths)){
+            size = init.filter(Files::isRegularFile).mapToLong(a -> {
+                try {
+                    return Files.size(a);
+                }catch (IOException e){
+
+                    e.getStackTrace();
+                    return 0l;
+                }
+            }).sum();
+        }catch (IOException ex){
+            ex.getStackTrace();
+        }
+        return size;
     }
 }
--- 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner;
+    private static FileUtils fileUtils = new FileUtils();
+
     public static void main(String[] args) {
 
+        System.out.println("Введите путь до папки:");
+        scanner = new Scanner(System.in);
+        for(;;){
+            try {
+                System.out.println(fileUtils.calculateFolderSize(scanner.nextLine()) / (1024 * 1024) + " mb");
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }
     }
 }
