64315d4e922e1431aecbf1f18ea7d4ad7ad98ace
Roman <mxhx23@gmail.com>
2021-09-18 10:35:24 +0300

    13.3 задание

--- 13_FilesAndNetwork/homework_13.3/src/main/java/Movements.java
+++ 13_FilesAndNetwork/homework_13.3/src/main/java/Movements.java
@@ -5,74 +5,87 @@ import java.util.*;
 import java.util.regex.Pattern;
 
 public class Movements {
-    private static final String REX = "([А-ЯЁа-яё ]{12})(,)([0-9]{20})(,)([A-Z]{3})(,)([0-9.]{8})(,)([A-Z_0-9]*)(,)([0-9+]{16})([ ]*)([A-Z0-9]*|)([A-Za-z0-9 _>/\\\\]*)([0-9.]{8})( )([0-9.]{8})([ ]*)([0-9.]*)([ ]*)([A-Z]{3})( )([A-Za-z0-9- )(]*)(,)([0-9,]*)(\\\"|)([0-9]*|)(,|)([0-9]*|)(\\\"|)";
-    private static final double euroExchangeRate = 85.27;
-    private static final double dollarExchangeRate = 72.53;
-    private static HashMap<String, List<Double>> list = new HashMap<>();
+    private static final String REX = "^([А-ЯЁа-яёA-Za-z0-9,. _+]+)([\\/A-Za-z0-9 _>|\\\\A-Za-z0-9 ]*[ ])([0-9. ]{17})([ 0-9.]+)(RUR|EUR|USD)([A-Za-z0-9 ,.()-]+|)(\\\"|)([0-9,]+|)(\\\"|)";
+    private static HashMap<String, List<Double>> finalList = new HashMap<>();
 
     public Movements(String pathMovementsCsv) {
         try {
-            List<String> lines = Files.readAllLines(Paths.get(pathMovementsCsv));
-            List<String[]> strings = new ArrayList<>();
-            for (int i = 1; i < lines.size(); i++){
-                String[] str = Pattern.compile(REX)
-                        .matcher(lines.get(i))
-                        .replaceAll("$14#$19#$21#$25$27$29")
-                        .split("#");
-                strings.add(str);
-            }
-            for(int i = 0; i < strings.size();i++){
-                String name = strings.get(i)[0].replace("/", "").replace("\\", "").trim().toUpperCase();
-                double income;
-                double expense;
-                List<Double> doubles = new ArrayList<>();
-                if(strings.get(i)[2].equals("RUR")){
-                    income = Double.parseDouble(strings.get(i)[1].replace(",", "."));
-                    expense = Double.parseDouble("-" + strings.get(i)[3].replace(",", "."));
-                }else if(strings.get(i)[2].equals("EUR")){
-                    income = euroExchangeRate * Double.parseDouble(strings.get(i)[1].replace(",", "."));
-                    expense = euroExchangeRate * Double.parseDouble("-" + strings.get(i)[3].replace(",", "."));
-                }else if(strings.get(i)[2].equals("USD")){
-                    income = dollarExchangeRate * Double.parseDouble(strings.get(i)[1].replace(",", "."));
-                    expense = dollarExchangeRate * Double.parseDouble("-" + strings.get(i)[3].replace(",", "."));
-                }else {
-                    throw new Exception("Неизвестная валюта");
-                }
-                if(list.containsKey(name)){
-                    doubles = list.get(name);
-                }
-                doubles.add(income);
-                doubles.add(expense);
-                list.put(name, doubles);
-            }
+            List<String[]> strings = new ArrayList<>(getList(Files.readAllLines(Paths.get(pathMovementsCsv))));
+            finalList = getBigList(strings);
         }catch (Exception ex){
             ex.printStackTrace();
         }
-        //list.entrySet().stream().flatMap(a -> a.getValue().stream()).forEach(System.out::println);
 
     }
+
+    private static List<String[]> getList(List<String> str){
+        List<String[]> strings = new ArrayList<>();
+        for (int i = 1; i < str.size(); i++){
+            String[] str1 = Pattern.compile(REX)
+                    .matcher(str.get(i))
+                    .replaceAll("$2#$4#$5#$8")
+                    .split("#");
+            strings.add(str1);
+        }
+        return strings;
+    }
+
+    private static HashMap<String, List<Double>> getBigList(List<String[]> str){
+        HashMap<String, List<Double>> list = new HashMap<>();
+        for(int i = 0; i < str.size();i++){
+            String name = str
+                    .get(i)[0]
+                    .replace("/", "")
+                    .replace("\\", "")
+                    .trim()
+                    .toUpperCase();
+            List<Double> doubles = new ArrayList<>();
+            if(list.containsKey(name)){
+                doubles = list.get(name);
+            }
+            if (str.get(i)[2].equals("RUR")){
+                if (str.get(i)[0].trim().substring(0,1).equals("/")){
+                    doubles.add(Double.parseDouble(str.get(i)[1].trim()));
+                }else {
+                    doubles.add(Double.parseDouble("-" + str.get(i)[1].trim()));
+                }
+            }else {
+                if (str.get(i)[0].trim().substring(0,1).equals("/")){
+                    doubles.add(Double.parseDouble(str.get(i)[3].replace(",", ".")));
+                }else {
+                    doubles.add(Double.parseDouble("-" + str.get(i)[3].replace(",", ".")));
+                }
+            }
+            list.put(name, doubles);
+        }
+        return list;
+    }
+
     public double getExpenseSum() {
-        return Math.abs(list.entrySet()
+        return Math.abs(finalList.entrySet()
                 .stream()
                 .flatMap(a -> a.getValue()
                 .stream()
                 .filter(b -> b < 0))
-                .reduce((c1, c2) -> c1 + c2).get());
+                .mapToDouble(Double::doubleValue)
+                .sum());
     }
 
     public double getIncomeSum() {
-        return list.entrySet()
+
+        return finalList.entrySet()
                 .stream()
                 .flatMap(a -> a.getValue()
                 .stream()
                 .filter(b -> b > 0))
-                .reduce((c1, c2) -> c1 + c2).get();
+                .mapToDouble(Double::doubleValue)
+                .sum();
     }
 
     public void getOrgExp(){
         System.out.println("Суммы расходов по организациям:");
-        for (Map.Entry<String, List<Double>> entry:list.entrySet()){
-            System.out.println(entry.getKey() + "\t\t" + new DecimalFormat("#0.00").format(Math.abs(entry.getValue().stream().filter(a -> a < 0).reduce((b1, b2) -> b1 + b2).get())) + " руб.");
+        for (Map.Entry<String, List<Double>> entry:finalList.entrySet()){
+            System.out.println(entry.getKey() + "\t\t" + Math.abs(entry.getValue().stream().filter(g -> g < 0).mapToDouble(Double::doubleValue).sum()) + " руб.");
         }
     }
 
