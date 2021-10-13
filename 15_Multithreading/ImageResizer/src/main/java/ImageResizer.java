1eb938927c8e2d69fa4b48d3bb09736d4e94a3e4
mxhx23@gmail.com <s4S8cNxZR!NcAch>
2021-10-13 20:19:14 +0300

    15.1 доп_задание

--- 15_Multithreading/ImageResizer/src/main/java/ImageResizer.java
+++ 15_Multithreading/ImageResizer/src/main/java/ImageResizer.java
@@ -1,43 +1,38 @@
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.concurrent.Callable;
+
 import org.imgscalr.Scalr;
 
 
-public class ImageResizer implements Runnable{
-    private File[] files;
+public class ImageResizer {
+    private File file;
     private int newWidth;
     private String dstFolder;
 
-    public ImageResizer(File[] files, int newWidth, String dstFolder) {
-        this.files = files;
+    public ImageResizer(File file, int newWidth, String dstFolder) {
+        this.file = file;
         this.newWidth = newWidth;
         this.dstFolder = dstFolder;
-    }
 
-    @Override
-    public void run(){
         try {
-            for (File file : files) {
-                BufferedImage image = ImageIO.read(file);
-                if (image == null) {
-                    continue;
-                }
+            BufferedImage image = ImageIO.read(file);
 
-                int newHeight = (int) Math.round(
-                        image.getHeight() / (image.getWidth() / (double) newWidth)
-                );
+            int newHeight = (int) Math.round(
+                    image.getHeight() / (image.getWidth() / (double) newWidth)
+            );
 
-                BufferedImage newImage = resize(image, newWidth, newHeight);
+            BufferedImage newImage = resize(image, newWidth, newHeight);
 
-                File newFile = new File(dstFolder + "/" + file.getName());
-                ImageIO.write(newImage, "jpg", newFile);
-            }
+            File newFile = new File(dstFolder + "/" + file.getName());
+            ImageIO.write(newImage, "jpg", newFile);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         System.out.println("!");
     }
+
     public static BufferedImage resize(BufferedImage src, int targetWidth, int targetHeight){
         return Scalr.resize(src,
                 Scalr.Method.ULTRA_QUALITY,
--- 15_Multithreading/ImageResizer/src/main/java/Main.java
+++ 15_Multithreading/ImageResizer/src/main/java/Main.java
@@ -1,38 +1,21 @@
-import org.imgscalr.AsyncScalr;
-
 import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
-    final static int CORE_COUNT = 8;
 
     public static void main(String[] args) {
         String srcFolder = "src_images";
         String dstFolder = "dst_images";
         int newWidth = 300;
-        int count = 0;
-        int coreCount = 0;
-
-        boolean first = true;
 
         File[] files = new File(srcFolder).listFiles();
 
-        int onePart = files.length / CORE_COUNT;
-        int residual = files.length % CORE_COUNT;
-
-        for (int i = 0; i < files.length; i++){
-            if(count == onePart || first){
-                if(coreCount == CORE_COUNT - 1){
-                    onePart += residual;
-                }
-                File[] images = new File[onePart];
-                System.arraycopy(files, coreCount * count, images, 0, onePart);
-                ImageResizer resizer = new ImageResizer(images, newWidth, dstFolder);
-                new Thread(resizer).start();
-                coreCount++;
-                count = 0;
-                first = false;
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        service.submit(() -> {
+            for (int i = 0; i < files.length; i++){
+                new ImageResizer(files[i], newWidth, dstFolder);
             }
-            count++;
-        }
+        });
     }
 }
