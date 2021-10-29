8c80beed57a2fb0fc9329428f369f8a2b7e9f659
mxhx23@gmail.com <s4S8cNxZR!NcAch>
2021-10-29 13:37:56 +0300

    17.1 задание

---
+++ 17_SpringBootWebDevelopment/ToDoList/src/main/java/main/CalculateType.java
@@ -0,0 +1,5 @@
+package main;
+
+public enum CalculateType {
+    add, sub, mul, div
+}
--- 17_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
+++ 17_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
@@ -1,6 +1,8 @@
 package main;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
@@ -10,5 +12,22 @@ public class DefaultController {
     @RequestMapping("/")
     public String index(){
         return (new Date().toString());
+    }
+    @RequestMapping(value = "/calculator", params = {"a", "b", "type"}, method = RequestMethod.GET)
+    public int calculate(@RequestParam("a") int a,
+                         @RequestParam("b") int b,
+                         @RequestParam("type") CalculateType type
+    ){
+        if(type.equals(CalculateType.add)){
+            return a + b;
+        }else if(type.equals(CalculateType.sub)){
+            return a - b;
+        }else if(type.equals(CalculateType.mul)){
+            return a * b;
+        }else if(type.equals(CalculateType.div)){
+            return a / b;
+        }else {
+            return -999999999;
+        }
     }
 }
