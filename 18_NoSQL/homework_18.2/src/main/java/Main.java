8ce272a5dc775ced37094ceedc43e94848ac0da3
mxhx23@gmail.com <s4S8cNxZR!NcAch>
2021-11-10 12:18:56 +0300

    18.2 задание

--- 18_NoSQL/homework_18.2/src/main/java/Main.java
+++ 18_NoSQL/homework_18.2/src/main/java/Main.java
@@ -1,58 +1,48 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
 public class Main {
 
-    /*
-    // Запуск докер-контейнера:
-    // docker run --rm --name skill-redis -p 127.0.0.1:6379:6379/tcp -d redis
-
-    // Для теста будем считать неактивными пользователей, которые не заходили 2 секунды
-    private static final int DELETE_SECONDS_AGO = 2;
-
-    // Допустим пользователи делают 500 запросов к сайту в секунду
-    private static final int RPS = 500;
-
-    // И всего на сайт заходило 1000 различных пользователей
-    private static final int USERS = 1000;
-
-    // Также мы добавим задержку между посещениями
-    private static final int SLEEP = 1; // 1 миллисекунда
-
+    private static final int SLEEP = 1000;
+    private static final int MAX_USERS_ON_HOMEPAGE = 10;
     private static final SimpleDateFormat DF = new SimpleDateFormat("HH:mm:ss");
 
     private static void log(int UsersOnline) {
-        String log = String.format("[%s] Пользователей онлайн: %d", DF.format(new Date()), UsersOnline);
-        out.println(log);
+        String log = String.format("[%s] На главной странице показываем пользователя: %d", DF.format(new Date()), UsersOnline);
+        System.out.println(log);
     }
-    */
+    private static void log1(int UsersOnline) {
+        String log = String.format("[%s] Пользователь %d оплатил платную услугу", DF.format(new Date()), UsersOnline);
+        System.out.println(log);
+    }
+    private static void log2(int UsersOnline) {
+        String log = String.format("[%s] Пользователь %d удален с главной страницы", DF.format(new Date()), UsersOnline);
+        System.out.println(log);
+    }
 
     public static void main(String[] args) throws InterruptedException {
-
         RedisStorage redis = new RedisStorage();
         redis.init();
-        //redis.listKeys();
-
-        /*
+        int i = 1;
         while (true){
-
-        }
-
-         */
-
-        /*
-        RedisStorage redis = new RedisStorage();
-        redis.init();
-        // Эмулируем 10 секунд работы сайта
-        for(int seconds=0; seconds <= 10; seconds++) {
-            // Выполним 500 запросов
-            for(int request = 0; request <= RPS; request++) {
-                int user_id = new Random().nextInt(USERS);
-                redis.logPageVisit(user_id);
-                Thread.sleep(SLEEP);
+            int random = new Random().nextInt(10);
+            if(random == 0 && random != i && redis.homePageVisitTest(random)){
+                int user_id = new Random().nextInt(1000);
+                log1(user_id);
+                log(user_id);
+                redis.homePageVisit(user_id);
+            }else {
+                log(i);
+                redis.homePageVisit(i);
+                i++;
             }
-            redis.deleteOldEntries(DELETE_SECONDS_AGO);
-            int usersOnline = redis.calculateUsersNumber();
-            log(usersOnline);
+            while (redis.calculateUsersNumber() > MAX_USERS_ON_HOMEPAGE){
+                redis.justRemoveFirstUser();
+                log2(redis.firstUser());
+
+            }
+            Thread.sleep(SLEEP);
         }
-        redis.shutdown();
-        */
     }
 }
--- 18_NoSQL/homework_18.2/src/main/java/RedisStorage.java
+++ 18_NoSQL/homework_18.2/src/main/java/RedisStorage.java
@@ -4,11 +4,12 @@ import org.redisson.api.RScoredSortedSet;
 import org.redisson.api.RedissonClient;
 import org.redisson.client.RedisConnectionException;
 import org.redisson.config.Config;
+
+import javax.swing.text.html.HTMLDocument;
+import java.util.Arrays;
 import java.util.Date;
 
 public class RedisStorage {
-
-    /*
     // Объект для работы с Redis
     private RedissonClient redisson;
 
@@ -16,7 +17,7 @@ public class RedisStorage {
     private RKeys rKeys;
 
     // Объект для работы с Sorted Set'ом
-    private RScoredSortedSet<String> onlineUsers;
+    private RScoredSortedSet<String> homePage;
 
     private final static String KEY = "ONLINE_USERS";
 
@@ -28,71 +29,48 @@ public class RedisStorage {
     public void listKeys() {
         Iterable<String> keys = rKeys.getKeys();
         for(String key: keys) {
-            out.println("KEY: " + key + ", type:" + rKeys.getType(key));
+            System.out.println("KEY: " + key + ", type:" + rKeys.getType(key));
         }
     }
-
-    */
-
-    private RedissonClient redisson;
-    private RKeys rKeys;
-    private RScoredSortedSet<String> onlineUsers;
-    private final static String KEY = "ONLINE_USERS";
-
 
     void init() {
         Config config = new Config();
         config.useSingleServer().setAddress("redis://127.0.0.1:6379");
-        redisson = Redisson.create(config);
-        /*
         try {
-
+            redisson = Redisson.create(config);
         } catch (RedisConnectionException Exc) {
             System.out.println("Не удалось подключиться к Redis");
             System.out.println(Exc.getMessage());
         }
-        /*
-
         rKeys = redisson.getKeys();
-        //rKeys.getKeys().forEach(out::println);
-        onlineUsers = redisson.getScoredSortedSet(KEY);
+        homePage = redisson.getScoredSortedSet(KEY);
         rKeys.delete(KEY);
-
-        */
-    }
-
-    public void listKeys() {
-        Iterable<String> keys = rKeys.getKeys();
-        for(String key: keys) {
-            System.out.println("KEY: " + key + ", type:" + rKeys.getType(key));
-        }
     }
 
     void shutdown() {
         redisson.shutdown();
     }
 
-    /*
-
     // Фиксирует посещение пользователем страницы
-    void logPageVisit(int user_id)
+    void homePageVisit(int user_id)
     {
         //ZADD ONLINE_USERS
-        onlineUsers.add(getTs(), String.valueOf(user_id));
+        homePage.add(getTs(), String.valueOf(user_id));
+    }
+    boolean homePageVisitTest(int user_id)
+    {
+        return Arrays.stream(homePage.stream().toArray()).filter(a -> a.equals(user_id)).toArray().length == 0;
     }
 
-    // Удаляет
-    void deleteOldEntries(int secondsAgo)
-    {
-        //ZREVRANGEBYSCORE ONLINE_USERS 0 <time_5_seconds_ago>
-        onlineUsers.removeRangeByScore(0, true, getTs() - secondsAgo, true);
-
-
+    void  justRemoveFirstUser(){
+        homePage.remove(homePage.first());
+    }
+    int firstUser(){
+        return Integer.parseInt(homePage.first());
     }
     int calculateUsersNumber()
     {
         //ZCOUNT ONLINE_USERS
-        return onlineUsers.count(Double.NEGATIVE_INFINITY, true, Double.POSITIVE_INFINITY, true);
+        return homePage.count(Double.NEGATIVE_INFINITY, true, Double.POSITIVE_INFINITY, true);
     }
-    */
 }
