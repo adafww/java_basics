7ee21f6dd4e5dd70656f77ad83ec7c2774bc6d23
mxhx23@gmail.com <s4S8cNxZR!NcAch>
2021-11-16 13:37:19 +0300

    18.4 задание

--- 18_NoSQL/homework_18.4/src/main/java/Main.java
+++ 18_NoSQL/homework_18.4/src/main/java/Main.java
@@ -12,18 +12,25 @@ import com.mongodb.client.model.Projections;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
+import java.awt.image.LookupOp;
+import java.lang.invoke.MethodHandles;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Consumer;
 import java.util.regex.Pattern;
 
+import static com.mongodb.client.model.Aggregates.*;
+import static com.mongodb.client.model.Filters.eq;
+
 public class Main {
     public static void main(String[] args) {
 
         MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
         MongoDatabase database = mongoClient.getDatabase("local");
-        MongoCollection<Document> collection = database.getCollection("dbMarkets");
+        MongoCollection<Document> collectionMarkets = database.getCollection("marketsDb");
+        MongoCollection<Document> collectionProducts = database.getCollection("productsDb");
+        //MongoCollection<Document> collection = database.getCollection("db_18_1");
         Scanner scanner = new Scanner(System.in);
         Block<Document> printBlock = new Block<Document>() {
             @Override
@@ -32,57 +39,50 @@ public class Main {
             }
         };
 
-        //collection.drop();
-
+        //collectionMarkets.drop();
+        //collectionProducts.drop();
+        //collectionProducts.deleteOne(new BasicDBObject(new Document("productName", "Вода")
+         //       .append("markets", new Document("marketName", "Магнит"))));
 
         while (true){
             String[] input = scanner.nextLine().split(" ");
             if (input[0].equals("ВЫХОД") && input.length == 1) {
                 break;
-            }else if(input[0].equals("СТАТИСТИКА_ТОВАРОВ") && input.length == 1){
-                collection.find().forEach((Consumer<Document>) document -> {
+                //СТАТИСТИКА_ТОВАРОВ
+            }else if(input[0].equals("") && input.length == 1){
+                collectionMarkets.aggregate(Arrays.asList(
+                        //match(eq("marketName", "Магнит")),
+                        lookup("productsDb", "marketName", "markets", "products_list")
+                        //out("products")
+                )).forEach(printBlock);
+            }else if(input[0].equals("1") && input.length == 1){
+                collectionProducts.find().forEach((Consumer<Document>) document -> {
                     System.out.println(document.toJson());
                 });
-                System.out.println("-------------------");
-                collection.aggregate(
-                        Arrays.asList(
-                                Aggregates.match(Filters.eq("marketName", "Пятёрочка")),
-                                Aggregates.group("$connection", Accumulators.sum("count", 1))
-                                /*
-                                Aggregates.project(
-                                        Projections.fields(
-                                                Projections.excludeId(),
-                                                Projections.include("productName"),
-                                                Projections.computed(
-                                                        "aaa",
-                                                        new Document("products", 0)
-                                                )
-                                        )
-                                )
-                                */
-                        )
-                ).forEach(printBlock);
-
+            }else if(input[0].equals("2") && input.length == 1){
+                collectionMarkets.find().forEach((Consumer<Document>) document -> {
+                    System.out.println(document.toJson());
+                });
             }else if(input[0].equals("ВЫСТАВИТЬ_ТОВАР") && input.length == 3){
-                collection.insertOne(new Document("connection", new Document()
-                        .append("productName", input[1])
-                        .append("marketName", input[2])
-                ));
-                collection.find().forEach((Consumer<Document>) document -> {
+                collectionProducts.insertOne(
+                        new Document("productName", input[1])
+                                .append("markets", new Document("marketName", input[2]))
+                );
+                collectionProducts.find().forEach((Consumer<Document>) document -> {
                     System.out.println(document.toJson());
                 });
-
+                //ВЫСТАВИТЬ_ТОВАР Вода Магнит
             }else if(input[0].equals("ДОБАВИТЬ_ТОВАР") && input.length == 3){
-                collection.insertOne(new Document("products", new Document()
+                collectionProducts.insertOne(new Document()
                         .append("productName", input[1])
                         .append("count", Integer.parseInt(input[2]))
-                ));
-                collection.find().forEach((Consumer<Document>) document -> {
+                );
+                collectionProducts.find().forEach((Consumer<Document>) document -> {
                     System.out.println(document.toJson());
                 });
             }else if(input[0].equals("ДОБАВИТЬ_МАГАЗИН") && input.length == 2){
-                collection.insertOne(new Document("markets", new Document("marketName", input[1])));
-                collection.find().forEach((Consumer<Document>) document -> {
+                collectionMarkets.insertOne(new Document("marketName", input[1]));
+                collectionMarkets.find().forEach((Consumer<Document>) document -> {
                     System.out.println(document.toJson());
                 });
             }
